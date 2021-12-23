df3bc056060479659f4a6068fd983c6805d5a2f5
Ramakant Shakya <37220368+shakya2417@users.noreply.github.com>
2021-12-23 18:25:34 +0530

    remove comments

--- data_generator_funsd_bert.py
+++ data_generator_funsd_bert.py
@@ -28,13 +28,14 @@ def getitem_box_bert(dataset_instance, idx):
         "label": dataset_instance.inp_list[idx]['labels']
     }
 def getitem_box_chargrid(dataset_instance, idx):
+    
     return {
         "ocr_values": [cell.ocr_value
                        for cell in dataset_instance.inp_list[idx]['cells']],
         "feats": np.array(dataset_instance.inp_list[idx]['charset_feature']),
-        # "feats": torch.Tensor(self.inp_list[idx]['pos_feats']).unsqueeze(0),
         "label": dataset_instance.inp_list[idx]['labels']
     }
+
 
 def getitem_box_bow(dataset_instance, idx):
     return {
@@ -147,16 +148,16 @@ def get_1px_mask_1_px_label(dataset_instance, idx):
 
 
 def get_box_mask_box_label_word(dataset_instance, idx):
+    
     instance_dict = dataset_instance.inp_list[idx]
     cell_lists = instance_dict['cells_word']
     cell_lists_textline = instance_dict['cells']
     min_x, min_y, max_x, max_y, min_w, min_h = get_min_max_x_y_w_h(cell_lists)
-    new_max_x = int((max_x-min_x)/min_w)+1
-    new_max_y = int((max_y-min_y)/min_h)+1
-    scaling_ratios = [cell.w/len(cell.ocr_value) if len(cell.ocr_value)!=0 else 0  for cell in cell_lists]
-    mean_scaling_ratios = sum(scaling_ratios)/len(scaling_ratios)
-    scaling_ratios = [ele if ele!=0 else mean_scaling_ratios for ele in scaling_ratios]
-    #scaling_ratios = [cell.w/len(cell.ocr_value) for cell in cell_lists]
+    new_max_x = int((max_x-min_x)/min_w) + 1
+    new_max_y = int((max_y-min_y)/min_h) + 1
+    scaling_ratios = [cell.w / len(cell.ocr_value) if len(cell.ocr_value) != 0 else 0  for cell in cell_lists]
+    mean_scaling_ratios = sum(scaling_ratios) / len(scaling_ratios)
+    scaling_ratios = [ele if ele != 0 else mean_scaling_ratios for ele in scaling_ratios]
     min_scale = min(scaling_ratios)
     label_mask = np.zeros((new_max_y, new_max_x)).astype('uint8')
     inp_grid = np.zeros((instance_dict['charset_feature'][0].shape[-1],
@@ -166,10 +167,10 @@ def get_box_mask_box_label_word(dataset_instance, idx):
         new_y = int((cell.y - min_y)/min_h)
         new_w = max(int(cell.w/min_scale), 1)
         new_h = max(int(cell.h/min_h), 1)
-        ocr_len=len(cell.ocr_value) if len(cell.ocr_value)!=0 else new_w
-        per_char_width = max(int(new_w/ocr_len), 1)
+        ocr_len = len(cell.ocr_value) if len(cell.ocr_value) != 0 else new_w
+        per_char_width = max(int(new_w / ocr_len), 1)
         for j, char in enumerate(cell.ocr_value):
-            inp_grid[:, new_y:new_y+new_h,new_x+per_char_width*j:new_x+per_char_width*(j+1)] =\
+            inp_grid[:, new_y:new_y+new_h, new_x+per_char_width*j:new_x+per_char_width*(j+1)] =\
                 np.expand_dims(np.expand_dims(instance_dict['charset_feature'][cell_idx][j], -1), -1)
     for cell_idx, cell in enumerate(cell_lists_textline):
         new_x = int((cell.x - min_x)/min_w)
@@ -229,6 +230,7 @@ class FUNSDMaskDataLoader(Dataset):
             return_lists = self.getitem(idx)
         return return_lists
 
+    
 class FUNSDCharGridDataLoaderBoxMaskBoxLabel(FUNSDMaskDataLoader):
     def __init__(self, funsd_pickle_path, labels_dict=None):
         super(FUNSDCharGridDataLoaderBoxMaskBoxLabel, self).__init__(
